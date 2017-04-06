8aac54ed7c1267e3c30cf92551e0e9c42bd9fbc4
Ben Lopatin <ben@benlopatin.com>
2017-04-06 16:20:50 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -7,19 +7,13 @@ from setuptools import setup, find_packages
 import broadcasts
 
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
-    sys.exit()
-
-
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 
 setup(
     author="Ben Lopatin",
-    author_email="ben.lopatin@wellfireinteractive.com",
+    author_email="ben@benlopatin.com",
     name='django-site-broadcasts',
     version=broadcasts.__version__,
     description='A small Django app that displays temporary, '
@@ -29,16 +23,21 @@ setup(
     license='BSD License',
     platforms=['OS Independent'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Framework :: Django',
     ],
     install_requires=[
-        'Django>=1.0',
+        'Django>=1.8',
     ],
     packages=find_packages(),
     zip_safe=False,
